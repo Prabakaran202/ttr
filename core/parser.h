856e224eee8a9b4299cdbdb735b06a5dd3788cf0
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-07-14 16:05:20 +0530

    Update parser.h

--- core/parser.h
+++ core/parser.h
@@ -4,19 +4,24 @@
 #include "lexer.h"
 
 typedef enum {
-    AST_PRINT,
-    AST_ASSIGNMENT // புதியது: வேரியபிள் அசைன்மெண்ட்
+    AST_PRINT,        // print("Hello")
+    AST_PRINT_VAR,    // print(a) - புதியது!
+    AST_ASSIGNMENT    // a = 12
 } ASTNodeType;
 
 typedef struct {
     ASTNodeType type;
-    char value[256]; 
-    
-    // Variables-க்காக புதிதாகச் சேர்க்கப்பட்டவை
-    char var_name[256]; // உதா: age
-    char var_type[10];  // உதா: Num, Str
+    char value[256];
+    char var_name[256];
+    char var_type[10];
 } ASTNode;
 
-ASTNode parse();
+// பல வரிகளைச் சேமிக்க ஒரு Array (List of AST Nodes)
+typedef struct {
+    ASTNode nodes[100];
+    int count;
+} ASTProgram;
+
+ASTProgram parse();
 
 #endif
