2992ebf566e4291a5c89a76f9bf20d0adeff117c
PRABAKARAN <140891635+Prabakaran202@users.noreply.github.com>
2026-07-14 16:06:51 +0530

    Update codegen.c
    update

--- src/codegen.c
+++ src/codegen.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include "../core/codegen.h"
 
-void generate_tamizhi_code(ASTNode node, const char *output_filename) {
+void generate_tamizhi_code(ASTProgram program, const char *output_filename) {
     FILE *fout = fopen(output_filename, "w");
     if (!fout) return;
 
     fprintf(fout, "main {\n");
 
-    if (node.type == AST_PRINT) {
-        fprintf(fout, "    call \"echo '%s'\" ;\n", node.value);
-    } 
-    // புதியது: Variable என்றால் தமிழி சிண்டாக்ஸில் எழுத
-    else if (node.type == AST_ASSIGNMENT) {
-        fprintf(fout, "    %s %s = %s ;\n", node.var_type, node.var_name, node.value);
+    // பதிவான அத்தனை வரிகளையும் (Array) வரிசையாக தமிழிக்கு மாற்று!
+    for (int i = 0; i < program.count; i++) {
+        ASTNode node = program.nodes[i];
+        
+        if (node.type == AST_PRINT) {
+            fprintf(fout, "    print(\"%s\");\n", node.value);
+        } 
+        else if (node.type == AST_PRINT_VAR) {
+            // நீங்க nano-ல காட்டிய அதே சிண்டாக்ஸ்!
+            fprintf(fout, "    print(%s);\n", node.var_name); 
+        } 
+        else if (node.type == AST_ASSIGNMENT) {
+            fprintf(fout, "    %s %s = %s ;\n", node.var_type, node.var_name, node.value);
+        }
     }
 
     fprintf(fout, "}\n");
-    
-    // Footer பிளாக்
     fprintf(fout, "\nfooter {\n    // End of program\n}\n");
 
     fclose(fout);
